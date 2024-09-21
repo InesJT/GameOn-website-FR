822ccf21858d18ef2a2150826f38ecf465f53e3b
Ines Trabelsi <ines.jenfaoui@gmail.com>
2024-09-21 12:39:01 +0200

    fix: issue #2 implement form entries

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,4 +29,26 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
+function validate() {
+  // validate first name field
+  const firstValue = document.getElementById("first").value;
+  if (firstValue.length < 2) return false;
+  // validate last name field
+  const lastValue = document.getElementById("last").value;
+  if (lastValue.length < 2) return false;
+  // validate email field
+  const emailValue = document.getElementById("email").value;
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) return false;
+  // validate quantity field
+  const quantityValue = document.getElementById("quantity").value;
+  if (quantityValue.length === 0) return false;
+  // validate that at least one radio button for location is selected
+  const locationBtns = document.getElementsByName("location");
+  let locationSelected = false;
+  locationBtns.forEach((btn) => locationSelected = locationSelected || btn.checked);
+  if (!locationSelected) return false;
+  // validate that the general conditions are read and accepted
+  const generalConditions = document.getElementById("checkbox1");
+  if (!generalConditions.checked) return false;
+  return true;
+}
