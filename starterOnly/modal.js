fc28f21505c487b45d6e3bfa915b2d6b76c32239
Ines Trabelsi <ines.jenfaoui@gmail.com>
2024-09-25 21:36:28 +0200

    fix: issue OpenClassrooms-Student-Center#4 add confirmation message on change

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -60,40 +60,98 @@ function showHideLocationError(field, isValid) {
   }
 }
 
-function validate() {
-  // validate first name field
-  const firstField = document.getElementById("first");
-  let isValid = firstField.value.length >= 2;
-  showHideError(firstField, firstField.value.length >= 2);
-  // validate last name field
-  const lastField = document.getElementById("last");
-  isValid = isValid && lastField.value.length >= 2;
-  showHideError(lastField, lastField.value.length >= 2);
-  // validate email field
-  const emailField = document.getElementById("email");
-  isValid = isValid && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailField.value);
-  showHideError(emailField, /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailField.value));
+// validate first name & last name field
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+firstName.addEventListener('change', function(e){
+  e.preventDefault();
+  validateName(firstName);
+});
+
+lastName.addEventListener('change', function(e){
+  e.preventDefault();
+  validateName(lastName);
+});
+
+function validateName(field){
+  // Définition du RegExp
+  let nameRegExp = new RegExp('^[A-Z,a-z]{2,30}$', 'g');
+  let isValid = field.value.length >= 2 && nameRegExp.test(field.value);
+  showHideError(field, isValid);
+  return isValid;
+}
+// validate email field
+const emailField = document.getElementById("email");
+emailField.addEventListener('change', function(e){
+  e.preventDefault();
+  validateEmail(emailField);
+});
+
+function validateEmail(field){
+  // Définition du RegExp
+  let isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(field.value);
+  showHideError(field, isValid);
+  return isValid;
+}
+
   // validate birthdate field
   const birthdateField = document.getElementById("birthdate");
-  isValid = isValid && birthdateField.value.length > 0;
-  showHideError(birthdateField, birthdateField.value.length > 0)
-  // validate quantity field
-  const quantityField = document.getElementById("quantity");
-  isValid = isValid && quantityField.value.length > 0;
-  showHideError(quantityField, quantityField.value.length > 0)
-  // validate that at least one radio button for location is selected
-  const locationBtns = document.getElementsByName("location");
-  let locationSelected = false;
-  locationBtns.forEach((btn) => locationSelected = locationSelected || btn.checked);
-  isValid = isValid && locationSelected;
-  showHideLocationError(locationBtns, locationSelected);
-  // validate that the general conditions are read and accepted
-  const generalConditions = document.getElementById("checkbox1");
-  isValid = isValid && generalConditions.checked;
-  showHideError(generalConditions, generalConditions.checked);
-  if (isValid) {
-    closeModal();
-    showThanks();
+  birthdateField.addEventListener('change', function(e){
+    e.preventDefault();
+    validateDate(birthdateField);
+  });
+  
+  function validateDate(field){
+    // Définition du RegExp
+    let dateRegExp = new RegExp('^(19|20)[0-9]{2}[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$' );
+    let isValid = dateRegExp.test(field.value);
+    showHideError(field, isValid);
+    return isValid;
   }
-  return false;
+
+    // validate quantity field
+    const quantityField = document.getElementById("quantity");
+    quantityField.addEventListener('change', function(e){
+      e.preventDefault();
+      validateQty(quantityField);
+    });
+    
+    function validateQty(field){
+      // Définition du RegExp
+      let isValid = quantityField.value.length > 0;
+      showHideError(field, isValid);
+      return isValid;
+    }
+
+    function validateLocations(){
+      // validate that at least one radio button for location is selected
+      const locationBtns = document.getElementsByName("location");
+      // Définition du RegExp
+      let locationSelected = false;
+      locationBtns.forEach((btn) => locationSelected = locationSelected || btn.checked);
+      showHideLocationError(locationBtns, locationSelected);
+      return locationSelected;
+    }    
+    
+    function validateConditions(){
+      // validate that the general conditions are read and accepted
+      const generalConditions = document.getElementById("checkbox1");
+      let isValid = generalConditions.checked;
+      showHideError(generalConditions, isValid);
+      return isValid;
+    } 
+
+function validate() {
+
+  if(validateName(firstName) && validateName(lastName) && validateEmail(emailField) &&
+  validateDate(birthdateField) && validateQty(quantityField) && validateLocations()
+  && validateConditions()){
+      // submit the form 
+      closeModal();
+      showThanks();
+  }
+  else{
+    alert("Veuillez vérifier tout le formulaire!! ");
+  }
+  return false; 
 }
